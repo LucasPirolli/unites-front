14881e50267d711ba7f9aede1803b1d52b323d81
Lucas Pirolli <lucas.pirolli@outlook.com>
2024-06-03 22:18:45 -0300

    feat: done layout academic productions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
+// Componentes UNITER
 import Topbar from "../components/Topbar";
+
+// Componentes de terceiros
+import PersonSearchIcon from "@mui/icons-material/PersonSearch";
+import InventoryIcon from "@mui/icons-material/Inventory";
+
+// Funções de terceiros
+import { useNavigate } from "react-router-dom";
 
 // Estilos UNITES
 import "../dist/scss/pages/home.scss";
 
-import PersonSearchIcon from "@mui/icons-material/PersonSearch";
-import InventoryIcon from '@mui/icons-material/Inventory';
-
 const Home = () => {
+  const navigate = useNavigate();
   const infoCards = [
     {
       icon: <PersonSearchIcon />,
@@ -19,17 +25,30 @@ const Home = () => {
     {
       icon: <InventoryIcon />,
       title: "Produções",
-      description: "A produção é definida como uma atividade ou processo que garante que um serviço, produto ou mesmo objeto seja feito/produzido."
-    }
-
+      description:
+        "A produção é definida como uma atividade ou processo que garante que um serviço, produto ou mesmo objeto seja feito/produzido.",
+    },
   ];
+
+  const handleTogglePage = (page) => {
+    page === "Pesquisadores"
+      ? navigate("/academic_productions")
+      : navigate("/academic_productions");
+  };
+
   return (
     <>
       <Topbar />
       <div className="container-home">
         {infoCards.map((item, index) => {
           return (
-            <div className="card" key={index}>
+            <div
+              className="card"
+              key={index}
+              onClick={() => {
+                handleTogglePage(item.title);
+              }}
+            >
               <div className="content-icon">{item.icon}</div>
               <div className="content">
                 <span className="title">{item.title}</span>
